e0b5913ec7ddd3b26b97511cc5ab27eab6a65110
Marcy Silverman <marcysilve225@gmail.com>
2019-05-06 12:00:32 -0700

    able to style the feature layers with icons

--- arcgis.js
+++ arcgis.js
@@ -3,7 +3,8 @@ require([
   "esri/Map",
   "esri/views/MapView",
   "esri/widgets/BasemapGallery",
-  "esri/layers/FeatureLayer"
+  "esri/layers/FeatureLayer",
+  // "esri/views/SceneView"
 ], function(Map, MapView, BasemapGallery, FeatureLayer) {
 
 var map = new Map({
@@ -27,15 +28,63 @@ var basemapGallery = new BasemapGallery({
   }
 });
 
-var featureLayer = new FeatureLayer({
-  url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+var trailheadsRenderer = {
+  "type": "simple",
+  "symbol": {
+    "type": "picture-marker",
+    "url": "http://static.arcgis.com/images/Symbols/NPS/npsPictograph_0231b.png",
+    "width": "18px",
+    "height": "18px"
+  }
+}
+var trailheadsLabels = {
+  symbol: {
+    type: "text",
+    color: "#FFFFFF",
+    haloColor: "#5E8D74",
+    haloSize: "2px",
+    font: {
+      size: "12px",
+      family: "noto-sans",
+      style: "italic",
+      weight: "normal"
+    }
+  },
+  labelPlacement: "above-center",
+  labelExpressionInfo: {
+    expression: "$feature.TRL_NAME"
+  }
+};
+
+var trailheads = new FeatureLayer({
+  url:
+    "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0",
+  renderer: trailheadsRenderer,
+  labelingInfo: [trailheadsLabels]
 });
 
-map.add(featureLayer);
-view.ui.add(basemapGallery, "top-right");
-  var featureLayer = new FeatureLayer({
-  url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
-});
+map.add(trailheads);
 
-map.add(featureLayer);
+// map.add(featureLayer);
+// view.ui.add(basemapGallery, "top-right");
+//   var featureLayer = new FeatureLayer({
+//   url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+// });
+
+// map.add(trailheads);
+
+// var view = new SceneView({
+//   container: "viewDiv",
+//   map: map,
+//   center: [-118.71511,34.09042],
+//   zoom: 15,
+//   camera: {
+//     tilt: 65,
+//     position: {
+//       x: -118.71,
+//       y: 33.75,
+//       z: 25000 // meters
+//     }
+//   }
+// });
 });
